0300c76b31c48ab31088f67f34483285e7c842f2
MauroDagnino <mdagnino@udesa.edu.ar>
2025-11-13 23:30:48 -0300

    index.js

---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function(){
+
+    let productosTop = document.querySelectorAll(".productostop")
+
+    let url = "https://dummyjson.com/products/category"
+
+    fetch(URL)
+    .then(function (res) {
+        return res.json();
+    })
+    
+    .then(function (data){
+        let resultados = data.products
+        console.log(resultados)
+
+        for (let i = 0; i < resultados.length; i++) {
+            let p = resultados[i];
+            productosTop.innerHTML += <article>
+                <img src="${p.thumbnail}"></img>
+                <h3>${p.title}</h3>
+                <p>$ ${p.price}</p>
+                <p>$ ${p.category}</p>
+                <p>${p.description}</p>
+            </article>
+        }
+    })
+    .catch(function (error) {
+       console.log("Error: " + error);
+    });
+})
