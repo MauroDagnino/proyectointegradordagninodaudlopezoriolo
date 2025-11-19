57c4165e47f8f643ebcf8d13cff6d00df7242c36
AndresLopezOriolo <alopezoriolo@udesa.edu.ar>
2025-11-19 14:10:54 -0300

    register y category

--- js/category.js
+++ js/category.js
@@ -41,4 +41,42 @@ fetch(urlCategory)
     console.log("Error: " + error);
 })
 
+
+
+let queryString = location.search;
+let query = new URLSearchParams(queryString);
+let category = query.get("cat");
+let product = document.querySelector(".productostop");
+
+product.innerHTML = ""
+
+let URL = `https://dummyjson.com/products/category/${category}`;
+
+fetch(URL)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        let listaProductos = data.products;
+
+        for (let i = 0; i < listaProductos.length; i++) {
+            let producto = listaProductos[i];
+
+            product.innerHTML += `
+                <article class="articulo_categoria">
+                    <img src="${producto.images[0]}" alt="${producto.title}">
+                    <h3>${producto.title}</h3>
+                    <p>$${producto.price}</p>
+                    <p>${producto.description}</p>
+                    <a href="./product.html?detalles=${producto.title}">Ver Detalles</a>
+                </article>
+            `;
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
 })
