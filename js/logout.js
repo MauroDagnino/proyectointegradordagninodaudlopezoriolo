b57a480333f4bdc601b70762a7db4750357453ad
ldaud-lab <ldaud@udesa.edu.ar>
2025-11-21 10:33:01 -0300

    logout otra

--- js/logout.js
+++ js/logout.js
@@ -18,8 +18,8 @@ document.addEventListener("click", function (e) {
             if (barra) {
                 barra.innerHTML = `<div class="login">
                 <a href="./login.html">Login</a>
-                <a href="./register.html">Registrarse</a><
-                /div>`
+                <a href="./register.html">Registrarse</a>
+                </div>`
             }
 
             window.location.href = "./index.html"
