30f9397ab0badb666785740e3bf1c27f972c435b
ldaud-lab <ldaud@udesa.edu.ar>
2025-11-21 09:07:33 -0300

    logout

---
+++ js/logout.js
@@ -0,0 +1,28 @@
+document.addEventListener("click", function (e) {
+    let barra = document.querySelector(".barra")
+    let mensaje = document.querySelector(".mensaje")
+    let logout = document.querySelector("#logout")
+    let saludo = document.querySelector(".saludo")
+
+    if (logout) {
+        logout.addEventListener("click", function (e) {
+            e.preventDefault()
+            localStorage.clear()
+
+            if (saludo) {
+                saludo.innerText = ""
+            }
+            if (mensaje) {
+                mensaje.innerText = ""
+            }
+            if (barra) {
+                barra.innerHTML = `<div class="login">
+                <a href="./login.html">Login</a>
+                <a href="./register.html">Registrarse</a><
+                /div>`
+            }
+
+            window.location.href = "./index.html"
+        })
+    }
+})
